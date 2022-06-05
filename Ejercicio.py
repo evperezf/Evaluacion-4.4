bf65fde77e4b18614e917275868097189025fd64
Evelyn <pf.evelyn.l@gmail.com>
2022-06-05 18:44:35 -0400

    subiendo ejercicio

---
+++ Ejercicio.py
@@ -0,0 +1,16 @@
+op = 4
+
+while op !=5:
+    print("Bienvenido a Vuelos-Duoc en que lo podemos ayudar.")
+    print("*"*50)
+    print("1.- Ver asientos disponibles.")
+    print("2.- Comprar asientos.")
+    print("3.- Anular vuelo.")
+    print("4.- Modificar datos de pasajero.")
+    print("5.- Salir")
+    print("*"*50)
+    try:
+        op = int(input("Ingresar una opcion (1-5): "))
+        flag = False
+    except:
+        print("Debe ingresar un numero")
