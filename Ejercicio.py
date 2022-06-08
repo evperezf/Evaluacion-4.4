93fdea92002d37db38273da01ac9d9ad0f2c8d02
Evelyn <pf.evelyn.l@gmail.com>
2022-06-08 13:32:18 -0400

    array

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,3 +1,4 @@
+import numpy as np
 def asientosDisp(asiento):
     asiento = asiento - asComp
     return asiento
@@ -9,6 +10,14 @@ vip = 12
 asComp = 0
 op = 4
 
+asientosList = [str(x) for x in range(1,43)]
+asientoVend ="14" #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
+# asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor
+array =np.array(asientosList).reshape(7,6)
+for fila in range(7):
+    for asiento in range(6):
+        if asientoVend == array[fila][asiento]:
+            array[fila][asiento] = "X"
 while op !=5:
     print("Bienvenido a Vuelos-Duoc en que lo podemos ayudar.")
     print("*"*50)
