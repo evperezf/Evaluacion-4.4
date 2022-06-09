180796f0dc927534fba918146e90c5dc41c05207
fvaras92 <106790096+fvaras92@users.noreply.github.com>
2022-06-09 16:23:16 -0400

    final

--- Ejercicio.py
+++ Ejercicio.py
@@ -2,34 +2,26 @@ import numpy as np
 from sqlalchemy import desc
 bancoduoc = 0
 descuento = 0.15
-#def asientosDisp(asiento):
-    #asiento = asiento - asComp
-    #return asiento
-#def vueloAnulado(asiento):
-    #asiento = asiento + asComp
-    #return asiento
 def comprarAsientos():
     print("comprar asientos")
     print("favor ingresar datos")
     print("*"*30)
     nombrePasajero=(input("Nombre: "))
-    rutPasajero=str(input("Rut: "))
-    telefonoPasajero=int(input("Telefono: "))
-    bancoPasajero=str(input("Banco: "))
-    print("*"*30)
-
-#def asientosDisp():   
+    rutPasajero=""
+    while len(rutPasajero) != 10:
+        rutPasajero=str(input("Rut (con guion y digito verificador): "))    
+    telefonoPasajero= ""
+    while len(str(telefonoPasajero)) !=8:    
+        telefonoPasajero=int(input("Telefono: (9) "))   
 asientosList_Norm = [str(x) for x in range(1,31)]
-asientoVend = 0 #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
-# asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor   
+asientoVend = 0  
 arrayNorm =np.array(asientosList_Norm).reshape(5,6)
 for fila in range(5):
     for asiento in range(6):
         if asientoVend == arrayNorm[fila][asiento]:
             arrayNorm[fila][asiento] = "X"
 asientosList_Vip = [str(x) for x in range(31,43)]
-asientoVend = 0 #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
-# asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor   
+asientoVend = 0  
 arrayVip =np.array(asientosList_Vip).reshape(2,6)
 for fila in range(2):
     for asiento in range(6):
@@ -58,10 +50,17 @@ while op !=5:
             print("asientos disponibles: \n",normal, " Asientos Normales\n", vip, " Asientos Vip")
         if op == 2:
             comprarAsientos()
+            bancoPasajero = input("Banco: ")
+            if bancoPasajero == "bancoDuoc":
+                valorAVipdesc = valorAVp*descuento
+                valorAsndesc=valorAsn*descuento
+                valorAsn = valorAsn - valorAsndesc
+                valorAVp = valorAVp - valorAVipdesc
+                print("*"*30)
             asComp = int(input("¿que asiento desea comprar? (1. normal y 2. es vip):  "))
             if asComp == 1:
                 asNorm = int(input("indicar cantidad de asientos que comprara: "))
-                print(arrayNorm)
+                print(arrayNorm)                
                 cantAsn = asNorm*valorAsn
                 for i in range(asNorm):
                     asientoVend = input("eliga asientos: ")
@@ -70,10 +69,8 @@ while op !=5:
                             if asientoVend == arrayNorm[fila][asiento]:
                                 arrayNorm[fila][asiento] = "X"
                                 print(arrayNorm)
-                                print(arrayVip)
-                          #asNorm = int(input("favor elegir asientos entre el 1 y el 30?: "))   
-                          #asiento = asientosDisp(asiento)
-                print("total: ",asNorm,"  $",cantAsn)
+                                print(arrayVip)                        
+                print(" Total asientos comprados: ",asNorm,"\n","Monto total a pagar: ""$",cantAsn)
             if asComp == 2:
                 asVip = int(input("indicar cantidad de asientos que comprara: "))
                 print(arrayVip)
@@ -86,19 +83,34 @@ while op !=5:
                                 arrayVip[fila][asiento] = "X"
                                 print(arrayNorm)
                                 print(arrayVip)
+            print(" Total asientos comprados: ",asVip,"\n","Monto total a pagar: ""$",cantAsv)
         if op == 3:
-
-            for fila in range(5):
-                for asiento in range(6):
-                    if arrayNorm[fila][asiento] == "X":
-                        arrayNorm[fila][asiento] = asientoVend
-                        
-                        print(arrayNorm)
-                        print(arrayVip)
-            
+            asNorm = int(input("indicar cantidad de asientos a devolver: "))
+            for i in range(asNorm):
+                eliminar = input("eliga asientos: ")
+                count = 0 
+                if int(eliminar) <= 30:
+                    for fila in range(5):
+                        for asiento in range(6):
+                            count = count + 1
+                            if str(arrayNorm[fila][asiento]) == "X" and count == int(eliminar):
+                                arrayNorm[fila][asiento] = count                           
+                                print(arrayNorm)
+                                print(arrayVip)
+                else:
+                    for i in range(asVip):
+                        count = count + 30
+                        for fila in range(2):
+                            for asiento in range(6):
+                                count = count + 1
+                                if str(arrayVip[fila][asiento]) == "X" and count == int(eliminar):
+                                    arrayVip[fila][asiento] = count 
+                                    print(arrayNorm)
+                                    print(arrayVip)      
         if op == 4:
             print("*"*30) 
             comprarAsientos()
+            bancoPasajero = input("Banco: ")
             print("*"*30)    
     except:
         print("Debe ingresar un numero")
