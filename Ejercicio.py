db84c961f92abcab8c7c92a3cb0f02b80ca9e691
Evelyn <pf.evelyn.l@gmail.com>
2022-06-09 01:51:19 -0400

    FUNCIONA OP 3

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,23 +1,46 @@
 import numpy as np
-def asientosDisp(asiento):
-    asiento = asiento - asComp
-    return asiento
-def vueloAnulado(asiento):
-    asiento = asiento + asComp
-    return asiento
-asiento = 30
+from sqlalchemy import desc
+bancoduoc = 0
+descuento = 0.15
+#def asientosDisp(asiento):
+    #asiento = asiento - asComp
+    #return asiento
+#def vueloAnulado(asiento):
+    #asiento = asiento + asComp
+    #return asiento
+def comprarAsientos():
+    print("comprar asientos")
+    print("favor ingresar datos")
+    print("*"*30)
+    nombrePasajero=(input("Nombre: "))
+    rutPasajero=str(input("Rut: "))
+    telefonoPasajero=int(input("Telefono: "))
+    bancoPasajero=str(input("Banco: "))
+    print("*"*30)
+
+#def asientosDisp():   
+asientosList_Norm = [str(x) for x in range(1,31)]
+asientoVend = 0 #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
+# asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor   
+arrayNorm =np.array(asientosList_Norm).reshape(5,6)
+for fila in range(5):
+    for asiento in range(6):
+        if asientoVend == arrayNorm[fila][asiento]:
+            arrayNorm[fila][asiento] = "X"
+asientosList_Vip = [str(x) for x in range(31,43)]
+asientoVend = 0 #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
+# asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor   
+arrayVip =np.array(asientosList_Vip).reshape(2,6)
+for fila in range(2):
+    for asiento in range(6):
+        if asientoVend == arrayVip[fila][asiento]:
+            arrayVip[fila][asiento] = "X"
+valorAVp = 240000
+valorAsn = 78900
+normal = 30
 vip = 12
 asComp = 0
 op = 4
-
-asientosList = [str(x) for x in range(1,43)]
-asientoVend ="14" #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
-# asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor
-array =np.array(asientosList).reshape(7,6)
-for fila in range(7):
-    for asiento in range(6):
-        if asientoVend == array[fila][asiento]:
-            array[fila][asiento] = "X"
 while op !=5:
     print("Bienvenido a Vuelos-Duoc en que lo podemos ayudar.")
     print("*"*50)
@@ -30,21 +53,53 @@ while op !=5:
     try:
         op = int(input("Ingresar una opcion (1-5): "))
         if op == 1:
-            print("asientos disponibles: \n",asiento, " Asientos Normales\n", vip, " Asientos Vip")
+            print(arrayNorm)
+            print(arrayVip)
+            print("asientos disponibles: \n",normal, " Asientos Normales\n", vip, " Asientos Vip")
         if op == 2:
-            asComp = int(input("¿Cuantos asientos desea comprar?: "))
-            asiento = asientosDisp(asiento)
-            print("asientos disponibles: ",asiento)
+            comprarAsientos()
+            asComp = int(input("¿que asiento desea comprar? (1. normal y 2. es vip):  "))
+            if asComp == 1:
+                asNorm = int(input("indicar cantidad de asientos que comprara: "))
+                print(arrayNorm)
+                cantAsn = asNorm*valorAsn
+                for i in range(asNorm):
+                    asientoVend = input("eliga asientos: ")
+                    for fila in range(5):
+                         for asiento in range(6):
+                            if asientoVend == arrayNorm[fila][asiento]:
+                                arrayNorm[fila][asiento] = "X"
+                                print(arrayNorm)
+                                print(arrayVip)
+                          #asNorm = int(input("favor elegir asientos entre el 1 y el 30?: "))   
+                          #asiento = asientosDisp(asiento)
+                print("total: ",asNorm,"  $",cantAsn)
+            if asComp == 2:
+                asVip = int(input("indicar cantidad de asientos que comprara: "))
+                print(arrayVip)
+                cantAsv = asVip*valorAVp
+                for i in range(asVip):
+                    asientoVend = input("eliga asientos: ")
+                    for fila in range(2):
+                         for asiento in range(6):
+                             if asientoVend == arrayVip[fila][asiento]:
+                                arrayVip[fila][asiento] = "X"
+                                print(arrayNorm)
+                                print(arrayVip)
         if op == 3:
-            anular = int(input("¿desea anular vuelo? (1=si, 2=no): "))
-            if anular == 1:
-                asiento = vueloAnulado(asiento)
-                print("asientos disponibles: ",asiento)
-            if anular == 2:
-                print("gracias por no anular")
+
+            for fila in range(5):
+                for asiento in range(6):
+                    if arrayNorm[fila][asiento] == "X":
+                        arrayNorm[fila][asiento] = asientoVend
+                        
+                        print(arrayNorm)
+                        print(arrayVip)
+            
         if op == 4:
-            print("continuara...")
-        
+            print("*"*30) 
+            comprarAsientos()
+            print("*"*30)    
     except:
         print("Debe ingresar un numero")
 print("adios")
--- prueba.py
+++ prueba.py
@@ -1,12 +1,13 @@
 import numpy as np
 
 asientosList = [str(x) for x in range(1,43)]
-asientoVend ="14" #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
+asientoVend = input("Que numero de asiento desea: ") #debe ser un opcion string? para que genere el cambio ya que todo debe ser extring ya que sino es imposible generar cuadraturas
 # asientoVend = input("Que asiento desea ocupar")//pero si es string habria que poner un lowercase para que tome todas las combinaciones posibles para generar un pago o no?, no necestariamente, se deberia generara un arametro del 1-30 para que el monto sea asiento normal y otro del 31-42 asinto vip para cobrar monto mayor
 array =np.array(asientosList).reshape(7,6)
 for fila in range(7):
     for asiento in range(6):
         if asientoVend == array[fila][asiento]:
             array[fila][asiento] = "X"
+           
             #for array in asientosList: # se debe pasar la array a string, pero como??
-print(array.flatten().tolist())
+print(array)
